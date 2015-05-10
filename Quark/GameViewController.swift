e03e8f5208cc3813f1c371438f5d4ac1a549ba0e
Conner Petzold <conner@getprismatic.com>
2015-05-09 23:31:16 -0700

    init

---
+++ Quark/GameViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let scene = GameScene(size: view.bounds.size)
+    scene.scaleMode = .AspectFill
+
+    let skView = self.view as! SKView
+//    skView.showsFPS = true
+//    skView.showsNodeCount = true
+    skView.ignoresSiblingOrder = true
+    skView.presentScene(scene)
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
+  override func prefersStatusBarHidden() -> Bool {
+    return true
+  }
+}
